c32a903879608166b30082e37cb153c9dab2738d
Kylapurity <111053205+Kylapurity@users.noreply.github.com>
2023-05-22 11:05:45 -0700

    Update env1.c

--- env1.c
+++ env1.c
@@ -33,6 +33,7 @@ int cmp_env_name(const char *nenv, const char *name)
  * Return: value of the environment variable if is found.
  * In other case, returns NULL.
  */
+
 char *_getenv(const char *name, char **_environ)
 {
 	char *ptr_env;
