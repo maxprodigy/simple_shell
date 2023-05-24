4e50c31aeeeedf7a88c578ffc4445a0685abfb6f
Kylapurity <111053205+Kylapurity@users.noreply.github.com>
2023-05-24 13:42:04 -0700

    Update aux_mem.c

--- aux_mem.c
+++ aux_mem.c
@@ -31,6 +31,7 @@ void _memcpy(void *newptr, const void *ptr, unsigned int size)
  * if new_size == old_size, returns ptr without changes.
  * if malloc fails, returns NULL.
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newptr;
